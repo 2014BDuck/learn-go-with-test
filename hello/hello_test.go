a65e80fa11805ef6793620b105d41d0791cb48f6
2014BDuck <jiekun.dev@gmail.com>
2021-01-31 20:30:02 +0800

    Refactor Hello

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -6,9 +6,26 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
-	if got != want {
-		t.Errorf("got '%q' want '%q'", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		// t.Helper() telling the test suite that this method is a helper
+		// line number reported will be in our function call
+		// rather than inside our test helper.
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
 	}
+	// subtests
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	// subtests
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
 }

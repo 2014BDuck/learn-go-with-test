8d27cf2c7cbd0797b96c948891b22237d2a897eb
2014BDuck <jiekun.dev@gmail.com>
2021-03-06 16:33:16 +0800

    Added mocking

---
+++ mocking/mocking.go
@@ -0,0 +1,59 @@
+// @Author: 2014BDuck
+// @Date: 2021/3/6
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const countdownStart = 3
+const finalWord = "Go!"
+const write = "write"
+const sleep = "sleep"
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+}
+
+type CountdownOperationsSpy struct {
+	Calls []string
+}
+
+func (o *ConfigurableSleeper) Sleep() {
+	time.Sleep(o.duration)
+}
+
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+func main() {
+	sleeper := &ConfigurableSleeper{1 * time.Second}
+	Countdown(os.Stdout, sleeper)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprintf(out, finalWord)
+}
